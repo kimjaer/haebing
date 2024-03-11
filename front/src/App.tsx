d02590dcdb28aa4aea83c91c4b1e79f3c9446e9d
kimjaer <wofus0422@gamil.com>
2024-03-11 13:56:42 +0900

    하단컴포넌트

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 // 컴포넌트
 import Hd from './components/Hd'
+import Ft from './components/Ft'
 
 // 스타일
 import 'bootstrap/dist/css/bootstrap.css'
@@ -14,9 +15,12 @@ function App() {
          제작노하우
          1. 컴포넌트의 재사용을 위해 id, class를 사용시 선택하도록 props 값으로 처리
          2. 디지털이미지들을 svg는 컴포넌트로 생성하여 css로 수정이 용이하도록 처리
+            핸드드로링의 유니끄함을 위해 메뉴텍스트를 특정 폰트가 아닌 svg로 처리
+         3. useState 없는 컴포넌트리스트 : Hd, Ft
       
       */}
       <Hd id="hd" cls="container d-flex flex-column justify-content-center align-items-center pt-4" />
+      <Ft id="ft" cls="mt-5"></Ft>
     </div>
   );
 }
--- front/src/components/Ft.js
+++ front/src/components/Ft.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import Good from '../img/component/Good'
+import Ftline from '../img/component/Ftline'
 
-function Ft() {
+function Ft({id, cls}) {
   return (
-    <div>Ft</div>
+    <footer id={id} className={cls}>
+       <Ftline></Ftline>
+       <div className='container pt-5'>
+       
+        <div className="d-flex align-items-center justify-content-center">
+          <Good />
+          <p className='ms-4'>
+            김재련의 카피라이터 A ~ Z 까지 모두 김재련의 손으로 창조되었음
+          </p>
+        </div>
+          
+       </div>
+    </footer>
+    
   )
 }
 
